9a2af3923dcc24f0fe2b6aacab1d4103bfb3b3b3
dbrockmann <dominiktobin@gmail.com>
2022-05-31 12:57:54 +0200

    feature: decoder fix #8

---
+++ model/decoder.py
@@ -0,0 +1,26 @@
+
+import tensorflow as tf
+
+class Decoder(tf.keras.Model):
+
+    def __init__(self):
+        """
+        Initializes the Decoder model layers
+        """
+
+        super(Decoder, self).__init__()
+
+        self.dense = tf.keras.layers.Dense(64)
+
+    @tf.function
+    def call(self, x, training):
+        """
+        Compute output of the model given an input
+
+        Args:
+            x: the input
+            training: flag stating if in training mode
+        """
+
+        x = self.dense(x, training=training)
+        return x
