71b36eb8e4816f8ef82c580dbb8b084e3f82c63e
fynnomenon <faurand@uni-osnabrueck.de>
2022-05-31 23:15:15 +0200

    change: variable names, comments fix #2

--- dataset.py
+++ dataset.py
@@ -15,23 +15,24 @@ def load_dataset(data_dir):
         labels: numpy array of shape(n_samples) containing the target values
     """
 
-    data_per_channel = list()
-    y = list()
+    data_per_recording = list()
+    labels_per_set = list()
     child_fold = ['F','N','O','S','Z']
     path_child_folds = [os.path.join(data_dir,x) for x in child_fold]
 
     for path in path_child_folds:
         files = [s for s in os.listdir(path)]
         for file in files:
-            # read the da
+            # read the data from the text files
             _data = pd.read_csv(os.path.join(path,file), engine = 'python', sep = "\r\n", header = None)
-            data_per_channel.append(_data.values.T)
+            data_per_recording.append(_data.values.T)
+        # label eeg recordings that contain seizures with 1, all others with 0 (binary classification)
         if 'S' in path:
-            y.append(np.ones((len(files),)))
+            labels_per_set.append(np.ones((len(files),)))
         else:
-            y.append(np.zeros((len(files),)))
+            labels_per_set.append(np.zeros((len(files),)))
 
-    data = np.concatenate(data_per_channel)
-    labels = np.concatenate(y, axis = 0)
+    data = np.concatenate(data_per_recording)
+    labels = np.concatenate(labels_per_set, axis = 0)
 
     return data, labels
