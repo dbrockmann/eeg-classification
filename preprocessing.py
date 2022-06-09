e31ae60d411090c2450a71fb985bd885c686f295
fynnomenon <faurand@uni-osnabrueck.de>
2022-06-09 14:17:49 +0200

    add: function that apllies preprocessing pipeline

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal import butter, lfilter
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 def butter_bandpass(lowcut, highcut, frequency, order=5):
     """
@@ -43,7 +44,7 @@ def butter_bandpass_filter(data, b, a):
 
     return y
 
-def scaler(data):
+def standard_scaler(data):
     """
     Design the scaler.
 
@@ -74,7 +75,7 @@ def standardize_data(scaler, data):
 
     return scaled_data
 
-def sliding_window_generator(data, windowsize):
+def sliding_window(data, windowsize):
     """
     Design a generator that iterates through 1d input array with
     predefiend chunks.
@@ -87,7 +88,7 @@ def sliding_window_generator(data, windowsize):
     for i in range(0, data.size, windowsize):
         yield data[i:i+windowsize]
 
-def sliding_window(data, labels, windowsize):
+def windowing(data, labels, windowsize):
     """
     Apply a non-overlapping sliding window.
 
@@ -98,21 +99,96 @@ def sliding_window(data, labels, windowsize):
             a divisor of the integer defining the size of the input array)
 
         Returns:
-            updated_data: numpy array of shape(num_of_recs_new, windowsize) containg the updated data vectors
-            updated_labels: numpy array of shape(num_of_recs_new) containing the updated target values
+            windowed_data: numpy array of shape(num_of_recs_new, windowsize) containg the windowed data vectors
+            windowed_labels: numpy array of shape(num_of_recs_new) containing the windowed target values
     """
     num_of_chunks = int(data.shape[1]/windowsize)
     num_of_recs_new = int(data.shape[0]*num_of_chunks)
 
-    updated_data = list()
-    updated_labels = np.zeros(num_of_recs_new,)
+    windowed_data = list()
+    windowed_labels = np.zeros(num_of_recs_new,)
 
     curr_chunks = 0
-    chunks = sliding_window_generator(data.flatten(), windowsize)
+    chunks = sliding_window(data.flatten(), windowsize)
 
     for chunk in chunks:
-        updated_data.append(chunk)
-        updated_labels[curr_chunks] = labels[curr_chunks//num_of_chunks]
+        windowed_data.append(chunk)
+        windowed_labels[curr_chunks] = labels[curr_chunks//num_of_chunks]
         curr_chunks += 1
 
-    return np.asarray(updated_data), updated_labels
+    return np.asarray(windowed_data), windowed_labels
+
+def split_dataset(X, y):
+    """
+    Split the data in a train, test and validation set using a 70:15:15 ratio.
+
+        Args:
+            X: n-dimensional input array containg the data vectors
+            y: 1d input array containg the target values
+
+        Returns:
+            data: list containg the training, test and validation sets of data vectors
+            labels: list containing the training, test and validation sets of target values
+    """
+    # split the dataset in train and test set while keeping the
+    # ratio between the target classes (as the data is imbalanced)
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        train_size=0.7,
+                                                        random_state=42,
+                                                        stratify=y)
+
+    # split the test set in test and validation set while keeping
+    # the ratio between the target classes
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test,
+                                                    train_size=0.5,
+                                                    random_state=42,
+                                                    stratify=y_test)
+
+    data = [X_train, X_test, X_val]
+    labels = [y_train, y_test, y_val]
+
+    return data, labels
+
+def prepare_data(X, y):
+    """
+    Apply all preproccesing steps to the specified data set. This includes splitting the
+    data into a training, test and validation set, filtering the data, standardizing the
+    data and finally the windowing of the data.
+
+        Args:
+            X: n-dimensional input array containg the data vectors
+            y: 1d input array containg the target values
+
+        Returns:
+            data: list containg the preprocessed training, test and validation sets of data vectors
+            labels: list containing the preprocessed training, test and validation sets of target values
+    """
+
+    # split the data in a train, test and validation set in a 70:15:15 ratio
+    data, labels = split_dataset(X, y)
+
+    # define the scaler (on the test set in order to prevent data leakage)
+    scaler = standard_scaler(data[0])
+
+    # define the filter coefficients for the butterworth band-pass filter
+    LOWCUT = 0.5
+    HIGHCUT = 40
+    SAMPLINGRATE = 173.61
+
+    b, a = butter_bandpass(LOWCUT,HIGHCUT,SAMPLINGRATE)
+
+    #define the windowsize
+    WINDOWSIZE = 241
+
+    prepared_data = list()
+    prepared_labels = list()
+
+    # apply the preprocessing pipeline to the different sets
+    for X, y in zip(data, labels):
+        filtered_X = butter_bandpass_filter(X,b,a)
+        standardized_X = standardize_data(scaler, filtered_X)
+        prepared_X, prepared_y = windowing(standardized_X, y, WINDOWSIZE)
+        prepared_data.append(prepared_X)
+        prepared_labels.append(prepared_y)
+
+    return prepared_data, prepared_labels
