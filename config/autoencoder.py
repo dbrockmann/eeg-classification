2a1dd8ffcebdba7d6d3ad4794f069c2b7635f16c
dbrockmann <dominiktobin@gmail.com>
2022-06-19 00:35:56 +0200

    feature: convolutional autoencoder config fix #59

--- config/autoencoder.py
+++ config/autoencoder.py
@@ -35,3 +35,74 @@ sparse_ae = {
     ]
 
 }
+
+convolutional_ae = {
+
+    'batch_size': 32,
+    'loss_function': tf.keras.losses.MeanSquaredError(),
+    'optimizer': tf.keras.optimizers.Adam(lr=0.001),
+    'epochs': 20,
+
+    'encoder': [
+        tf.keras.layers.Reshape(
+            (-1, 1)
+        ),
+        tf.keras.layers.Conv1D(
+            16, kernel_size=3, padding='same', activation='relu'
+        ),
+        tf.keras.layers.MaxPooling1D(
+            pool_size=2
+        ),
+        tf.keras.layers.Conv1D(
+            32, kernel_size=3, padding='same', activation='relu'
+        ),
+        tf.keras.layers.MaxPooling1D(
+            pool_size=2
+        ),
+        tf.keras.layers.Conv1D(
+            64, kernel_size=3, padding='same', activation='relu'
+        ),
+        tf.keras.layers.MaxPooling1D(
+            pool_size=2
+        ),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(
+            8, activation='relu',
+            activity_regularizer=tf.keras.regularizers.L1(0.001)
+        )
+    ],
+
+    'decoder': [
+        tf.keras.layers.Dense(
+            240 // 2**3 * 64
+        ),
+        tf.keras.layers.Reshape(
+            (240 // 2**3, 64)
+        ),
+        tf.keras.layers.Conv1DTranspose(
+            64, kernel_size=3, padding='same', activation='relu'
+        ),
+        tf.keras.layers.UpSampling1D(
+            size=2
+        ),
+        tf.keras.layers.Conv1DTranspose(
+            32, kernel_size=3, padding='same', activation='relu'
+        ),
+        tf.keras.layers.UpSampling1D(
+            size=2
+        ),
+        tf.keras.layers.Conv1DTranspose(
+            16, kernel_size=3, padding='same', activation='relu'
+        ),
+        tf.keras.layers.UpSampling1D(
+            size=2
+        ),
+        tf.keras.layers.Conv1D(
+            1, kernel_size=3, padding='same', activation='sigmoid'
+        ),
+        tf.keras.layers.Reshape(
+            (240,)
+        )
+    ]
+
+}
